a84e070cdd841350bd16e9d27751450f88fce240
Jasonlee6789 <jeffersonlee@163.com>
2020-12-27 10:44:49 +0800

    8个匹配器。

---
+++ .history/matchers.test_20201227103313.js
@@ -0,0 +1,3 @@
+const { TestScheduler } = require("jest");
+
+TestScheduler('测试007号技师的匹配', () => {  expect('').toBe()}
---
+++ .history/matchers.test_20201227103346.js
@@ -0,0 +1,5 @@
+const { TestScheduler } = require("jest");
+
+TestScheduler("测试007号技师的匹配", () => {
+  expect("007号技师").toBe("007号技师");
+});
